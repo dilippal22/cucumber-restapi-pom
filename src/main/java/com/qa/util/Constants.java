3f80fb448c9901e915ceb169288514e5fe8c6738
dilippal22 <dilippal22@gmail.com>
2022-07-25 22:35:08 +0530

    added constant

---
+++ src/main/java/com/qa/util/Constants.java
@@ -0,0 +1,18 @@
+package com.qa.util;
+
+/**
+ * @author Dilip Pal
+ *
+ */
+public class Constants {
+
+	public static final String baseURL = "https://bookstore.toolsqa.com";
+
+	public static final String tokenBasePathURI = "/Account/v1/GenerateToken";
+
+	public static final String listOfAvailableBooksPathURI = "/BookStore/v1/Books";
+
+	public static final String configPropertiesFilePath = System.getProperty("user.dir")
+			+ "\\src\\main\\java\\com\\qa\\config\\Config.properties";
+
+}
